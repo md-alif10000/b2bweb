69310eb1b4d8ca6a5989bfadcb82391343430e84
Md Alif <md.alif10000@gmail.com>
2022-01-28 18:22:40 +0600

    added swiper

--- pages/buyer.js
+++ pages/buyer.js
@@ -5,57 +5,56 @@ import Footer from "../src/components/Footer";
 const Buyer = () => {
   return (
     <>
-    <div className={styles.container}>
-      <PrimaryHeader />
+      <div className={styles.container}>
+        <PrimaryHeader />
 
-      <img className={styles.image1} src="images/dotted.png" alt="" />
-      <img className={styles.image2} src="images/rectangle1.png" alt="" />
-      <img className={styles.image3} src="images/rectangle2.png" alt="" />
+        <img className={styles.image1} src="images/dotted.png" alt="" />
+        <img className={styles.image2} src="images/rectangle1.png" alt="" />
+        <img className={styles.image3} src="images/rectangle2.png" alt="" />
 
+        <div>
+          <div className={styles.hero}>
+            <h1>
+              Tell us about your <span>Requirement</span>
+            </h1>
+            <p>Enter the details to get the best quality goods !</p>
+          </div>
+          <div className={styles.formContainer}>
+            <div className={styles.left}></div>
+            <div className={styles.right}>
+              <form action="" className={styles.form}>
+                <div className={styles.inputs}>
+                  <Input />
+                  <Input />
+                  <Input />
+                  <Input />
+                  <Input />
+                  <Input />
+                </div>
 
-      <div>
-        <div className={styles.hero}>
-          <h1>
-            Tell us about your <span>Requirement</span>
-          </h1>
-          <p>Enter the details to get the best quality goods !</p>
-        </div>
-        <div className={styles.formContainer}>
-          <div className={styles.left}></div>
-          <div className={styles.right}>
-            <form action="" className={styles.form}>
-              <div className={styles.inputs} >
-                <Input />
-                <Input />
-                <Input />
-                <Input />
-                <Input />
-                <Input />
-              </div>
-
-              <div className={styles.checkboxContainer}>
-                <input type="checkbox" />
-                <span>
-                  I agree to all the{" "}
-                  <a href="">Terms and Conditions & Policies</a>
-                </span>{" "}
-                <br />
-                <input type="checkbox" />
-                <span>
-                  I agree to all the{" "}
-                  <a href="">Terms and Conditions & Policies</a>
-                </span>{" "}
-                <br />
-              </div>
-              <button className={styles.submitButton}>
-                Post Buy Requirement
-              </button>
-            </form>
+                <div className={styles.checkboxContainer}>
+                  <input type="checkbox" />
+                  <span>
+                    I agree to all the{" "}
+                    <a href="">Terms and Conditions & Policies</a>
+                  </span>{" "}
+                  <br />
+                  <input type="checkbox" />
+                  <span>
+                    I agree to all the{" "}
+                    <a href="">Terms and Conditions & Policies</a>
+                  </span>{" "}
+                  <br />
+                </div>
+                <button className={styles.submitButton}>
+                  Post Buy Requirement
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
